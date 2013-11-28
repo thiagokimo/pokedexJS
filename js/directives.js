b218191f8b5c2ab0bb94a2c587f5884a242d5c9a
Raphael Faria <raphael@codelogic.me>
2013-11-28 02:30:07 -0200

    Adding pokemon info to url

--- js/directives.js
+++ js/directives.js
@@ -4,6 +4,6 @@ app.directive('pokemon', function () {
     scope: {
       pokemon: "=info"
     },
-    template: '<img src="{{pokemon.picture}}" alt=""><h1>{{pokemon.id}}</h1><h1>{{pokemon.identifier}}</h1><h1>{{pokemon.type}}</h1>'
+    templateUrl: '../pokemon-info.html'
   };
 });
