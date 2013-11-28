adc00c4a85a843a85f91be8a601d1186804600b0
Thiago Rocha <kimo@kimo.io>
2013-11-28 14:18:37 -0200

    Adding script that generate the database

---
+++ db/db_generator.rb
@@ -0,0 +1,28 @@
+require 'json_builder'
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("http://pokemondb.net/pokedex/game/firered-leafgreen"))
+
+pokemons = doc.css(".infocard-tall")
+
+json = JSONBuilder::Compiler.generate do
+
+  array pokemons do |pokemon|
+    name = pokemon.at_css(".ent-name").text
+    types = pokemon.css(".aside a").collect do |type|
+      type.text
+    end
+
+    number pokemon.at_css("small:nth-child(3)").text
+    name name
+    type types
+    sprite "http://img.pokemondb.net/sprites/black-white/normal/"+name.downcase+".png"
+  end
+end
+
+db = File.open("../js/pokemonDB.js", "w")
+# db.write("var db = ")
+db.write(json);
+# db.write(";")
+db.close
